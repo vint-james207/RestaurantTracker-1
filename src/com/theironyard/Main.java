5d561deac0decfda557a7f5b342826dfdeb4df74
oakes <zsoakes@gmail.com>
2016-06-07 12:01:10 -0400

    Add submit, view, delete

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -24,6 +24,8 @@ public class Main {
                         return new ModelAndView(m, "login.html");
                     }
                     else {
+                        User user = users.get(username);
+                        m.put("restaurants", user.restaurants);
                         return new ModelAndView(m, "home.html");
                     }
                 },
@@ -43,12 +45,71 @@ public class Main {
                         user = new User(name, pass);
                         users.put(name, user);
                     }
-                    else if (!name.equals(user.name)) {
+                    else if (!pass.equals(user.password)) {
                         throw new Exception("Wrong password");
                     }
 
                     Session session = request.session();
                     session.attribute("username", name);
+
+                    response.redirect("/");
+                    return "";
+                }
+        );
+        Spark.post(
+                "/create-restaurant",
+                (request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+                    if (username == null) {
+                        throw new Exception("Not logged in");
+                    }
+
+                    String name = request.queryParams("name");
+                    String location = request.queryParams("location");
+                    int rating = Integer.valueOf(request.queryParams("rating"));
+                    String comment = request.queryParams("comment");
+                    if (name == null || location == null || comment == null) {
+                        throw new Exception("Invalid form fields");
+                    }
+
+                    User user = users.get(username);
+                    if (user == null) {
+                        throw new Exception("User does not exist");
+                    }
+
+                    Restaurant r = new Restaurant(name, location, rating, comment);
+                    user.restaurants.add(r);
+
+                    response.redirect("/");
+                    return "";
+                }
+        );
+        Spark.post(
+                "/logout",
+                (request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return "";
+                }
+        );
+        Spark.post(
+                "/delete-restaurant",
+                (request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+                    if (username == null) {
+                        throw new Exception("Not logged in");
+                    }
+
+                    int id = Integer.valueOf(request.queryParams("id"));
+
+                    User user = users.get(username);
+                    if (id <= 0 || id - 1 >= user.restaurants.size()) {
+                        throw new Exception("Invalid id");
+                    }
+                    user.restaurants.remove(id - 1);
 
                     response.redirect("/");
                     return "";
